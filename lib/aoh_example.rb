e93fc59ac16d542db1f0ae29a09cb30926afa01a
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-15 19:47:56 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,11 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  [{:name=>"A", :occupation=>"B"},{:name=>"C", :occupation=>"D"}]
+  [{ name:  "Don Gately", occupation: "Live-in Staff" },
+   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+   { name:  "Pat Monteseian", occupation: "Staff" },
+   { name:  "Kate Gompert", occupation: "None" },
+   { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 def aoh_lookup(aoh, row, key)
